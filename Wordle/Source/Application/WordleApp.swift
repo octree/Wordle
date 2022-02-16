9d0178d92fcde7b82adcde9e46041620dddcbd05
Octree <fouljz@gmail.com>
2022-02-16 14:24:01 +0800

    🎉 Initial commit.

---
+++ Wordle/Source/Application/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Octree on 2022/2/16.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
